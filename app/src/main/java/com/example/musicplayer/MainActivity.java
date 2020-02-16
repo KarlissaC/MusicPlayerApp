57880534d799af9c22b898579a277754b5465151
KarlissaC <karlissacole2@gmail.com>
2020-02-16 15:23:15 -0500

    Initial commit

---
+++ app/src/main/java/com/example/musicplayer/MainActivity.java
@@ -0,0 +1,81 @@
+package com.example.musicplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button play,pause,stop;
+    MediaPlayer mediaPlayer;
+    int pauseCurrentPosition;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_main );
+
+        play=(Button) findViewById(R.id.btn_play);
+        pause=(Button) findViewById(R.id.btn_pause);
+        stop=(Button) findViewById(R.id.btn_stop);
+
+
+
+
+        play.setOnClickListener(this);
+        pause.setOnClickListener(this);
+        stop.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view)  {
+
+        switch (view.getId()) {
+
+            case R.id.btn_play:
+               if(mediaPlayer==null){
+                   mediaPlayer=MediaPlayer.create(getApplicationContext(),R.raw.nicki);
+                mediaPlayer.start(); }
+
+
+                else if(!mediaPlayer.isPlaying()) {
+                    mediaPlayer.seekTo(pauseCurrentPosition);
+                    mediaPlayer.start();
+
+                }
+
+
+
+                break;
+
+
+                case R.id.btn_pause:
+                if(mediaPlayer!=null) {
+                 mediaPlayer.pause();
+                 pauseCurrentPosition=mediaPlayer.getCurrentPosition();
+
+                }
+                break;
+
+                case R.id.btn_stop:
+
+                  if(mediaPlayer!=null) {
+                      mediaPlayer.stop();
+
+                      mediaPlayer = null;
+
+                  }
+
+                  break;
+
+
+
+
+
+        }
+    }
+}
